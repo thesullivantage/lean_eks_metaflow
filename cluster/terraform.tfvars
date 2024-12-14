3560ff71c4ac7b322b2e856e9c2ca6b2a28d3baf
thesullivantage <38443606+thesullivantage@users.noreply.github.com>
2024-12-14 04:07:03 -0500

    finish tidying

---
+++ cluster/terraform.tfvars
@@ -0,0 +1,5 @@
+// filepath: /c:/Users/jacks/OneDrive/Documents/Post2023/careerStuff/AI_company/eks_argo/cluster/terraform.tfvars
+
+cluster_name = "my-eks-cluster"
+vpc_id = "vpc-xxxxx"
+private_subnets = ["subnet-xxxxx", "subnet-yyyyy"]
--- resources/argo.tf
+++ resources/argo.tf
@@ -1,8 +1,7 @@
-provider "kubectl" {
+provider "kubernetes" {
   host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_ca_certificate)
   token                  = data.terraform_remote_state.eks.outputs.cluster_auth_token
-  load_config_file       = false
 }
 
 resource "kubernetes_namespace" "argo" {
